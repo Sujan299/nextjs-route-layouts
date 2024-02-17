e773fcc74f75b35f2dd0d469dcf2f418884a7a77
Sujan <mrsujan321@gmail.com>
2024-02-17 14:40:18 +0545

    fifth

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -2,5 +2,5 @@ import React from 'react'
 import styled from '../style.module.css'
 
 export default function User() {
-    return (<div className={styled.user}><h1>Welcome to user's dashboard</h1></ div>)
+    return (<div className={styled.user}><h1>Welcome to admins dashboard</h1></ div>)
 }
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import styled from '../style.module.css';
 export default function User() {
     return (<div className={styled.admin}>
-        <h1>Welcome to user's dashboard</h1>
+        <h1>Welcome to users dashboard</h1>
     </div>
     )
 }
